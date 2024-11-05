df65ffcb5b8bc69833ec5af2884df94f73f8eeb4
Aditya0o7 <adityasharmao371@gmail.com>
2024-11-05 14:20:58 +0530

    day5

---
+++ Day5/async1.js
@@ -0,0 +1,36 @@
+// sayhello=() => {
+//     console.log("Hello World!");
+// };
+// console.log('start');
+// setTimeout()=>{
+//     console.log('first");
+//     setTimeout()=>{
+//         setTimeout(() =>{
+//             };    
+// };
+// console.log('end');
+
+// const myPromise=new Promise((resolve,reject)=>{
+//     let success=true;
+//     if(success){
+//         resolve("Data send success");
+//     }
+//     else{
+//         reject=("Data failed to send");
+//     }
+// });
+// myPromise
+// .then((message)=>console.log(message))
+// .catch((error)=>("Error fetching data"+error));
+
+// function task(message,delay){
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             console.log(message);
+//             resolve();
+//         },delay);
+//     });
+// }
+// task("First task completed",1000)
+// .then(()=>task("Second task is completed",2000))
+// .then(()=>task("Third task is completed",3000)); 
