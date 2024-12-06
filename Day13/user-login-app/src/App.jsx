3853354c9af5ea7b6c7bbb19cd91f7e0f52366f2
Aditya Sharma <adityasharmao371@gmail.com>
2024-12-06 10:32:38 +0530

    Day13

--- Day13/user-login-app/src/App.jsx
+++ Day13/user-login-app/src/App.jsx
@@ -1,21 +1,26 @@
-import{BrowserRouter,Routes,Route} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./Components/MainLayout";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
+import DashBoard from "./Components/DashBoard";
+import Logout from "./Components/Logout";
+
 const App = () => {
-  const[data,setData]=useState();
+  const [data, setData] = useState();
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainLayout/>} >
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      </Route>
-      <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/logout" element={<Logout />} />
-    </Routes>
-    </BrowserRouter>
+      {JSON.stringify(data)}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register regData={setData} />} />
+          </Route>
+          <Route path="dashboard" element={<DashBoard />} />
+          <Route path="logout" element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- Day13/user-login-app/src/Components/Login.jsx
+++ Day13/user-login-app/src/Components/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+         <form>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email" />
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd" />
+  </div>
+  <br />
+  <button type="submit" class="btn btn-success">Login</button>
+</form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
